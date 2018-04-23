a813c749ea130d8e07aff0788e8abcec9db760eb
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/Connecting to Hierarchical Data/MainWindow.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+
+namespace Connecting_to_Hierarchical_Data {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+            grid.ItemsSource = Staff.GetStaff();
+            treeListView1.ExpandAllNodes();
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to: Bind to Self-Referential Data Structure
+
+
+<p>This example shows how to create a self referencing data structure and display it within the TreeListView.</p>
+
+<br/>
+
+
