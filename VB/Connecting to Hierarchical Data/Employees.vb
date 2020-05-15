6af5e2dccf77ccb6530679a5b9ba3a8ba5f7cf36
DevExpressExampleBot <fakeEmail>
2020-05-15 16:09:36 +0300

    Source auto update [skip ci]

--- VB/Connecting to Hierarchical Data/Employees.vb
+++ VB/Connecting to Hierarchical Data/Employees.vb
@@ -13,37 +13,36 @@ Namespace Connecting_to_Hierarchical_Data
 
 	Public Module Staff
 		Public Function GetStaff() As List(Of Employee)
-'INSTANT VB NOTE: The variable staff was renamed since it may cause conflicts with calls to static members of the user-defined type with this name:
-			Dim staff_Conflict As New List(Of Employee)()
-			staff_Conflict.Add(New Employee() With {
+			Dim CompanyStaff As New List(Of Employee)()
+			CompanyStaff.Add(New Employee() With {
 				.ID = 1,
 				.ParentID = 0,
 				.Name = "Gregory S. Price",
 				.Department = "",
 				.Position = "President"
 			})
-			staff_Conflict.Add(New Employee() With {
+			CompanyStaff.Add(New Employee() With {
 				.ID = 2,
 				.ParentID = 1,
 				.Name = "Irma R. Marshall",
 				.Department = "Marketing",
 				.Position = "Vice President"
 			})
-			staff_Conflict.Add(New Employee() With {
+			CompanyStaff.Add(New Employee() With {
 				.ID = 3,
 				.ParentID = 1,
 				.Name = "John C. Powell",
 				.Department = "Operations",
 				.Position = "Vice President"
 			})
-			staff_Conflict.Add(New Employee() With {
+			CompanyStaff.Add(New Employee() With {
 				.ID = 4,
 				.ParentID = 1,
 				.Name = "Christian P. Laclair",
 				.Department = "Production",
 				.Position = "Vice President"
 			})
-			staff_Conflict.Add(New Employee() With {
+			CompanyStaff.Add(New Employee() With {
 				.ID = 5,
 				.ParentID = 1,
 				.Name = "Karen J. Kelly",
@@ -51,28 +50,28 @@ Namespace Connecting_to_Hierarchical_Data
 				.Position = "Vice President"
 			})
 
-			staff_Conflict.Add(New Employee() With {
+			CompanyStaff.Add(New Employee() With {
 				.ID = 6,
 				.ParentID = 2,
 				.Name = "Brian C. Cowling",
 				.Department = "Marketing",
 				.Position = "Manager"
 			})
-			staff_Conflict.Add(New Employee() With {
+			CompanyStaff.Add(New Employee() With {
 				.ID = 7,
 				.ParentID = 2,
 				.Name = "Thomas C. Dawson",
 				.Department = "Marketing",
 				.Position = "Manager"
 			})
-			staff_Conflict.Add(New Employee() With {
+			CompanyStaff.Add(New Employee() With {
 				.ID = 8,
 				.ParentID = 2,
 				.Name = "Angel M. Wilson",
 				.Department = "Marketing",
 				.Position = "Manager"
 			})
-			staff_Conflict.Add(New Employee() With {
+			CompanyStaff.Add(New Employee() With {
 				.ID = 9,
 				.ParentID = 2,
 				.Name = "Bryan R. Henderson",
@@ -80,28 +79,28 @@ Namespace Connecting_to_Hierarchical_Data
 				.Position = "Manager"
 			})
 
-			staff_Conflict.Add(New Employee() With {
+			CompanyStaff.Add(New Employee() With {
 				.ID = 10,
 				.ParentID = 3,
 				.Name = "Harold S. Brandes",
 				.Department = "Operations",
 				.Position = "Manager"
 			})
-			staff_Conflict.Add(New Employee() With {
+			CompanyStaff.Add(New Employee() With {
 				.ID = 11,
 				.ParentID = 3,
 				.Name = "Michael S. Blevins",
 				.Department = "Operations",
 				.Position = "Manager"
 			})
-			staff_Conflict.Add(New Employee() With {
+			CompanyStaff.Add(New Employee() With {
 				.ID = 12,
 				.ParentID = 3,
 				.Name = "Jan K. Sisk",
 				.Department = "Operations",
 				.Position = "Manager"
 			})
-			staff_Conflict.Add(New Employee() With {
+			CompanyStaff.Add(New Employee() With {
 				.ID = 13,
 				.ParentID = 3,
 				.Name = "Sidney L. Holder",
@@ -109,21 +108,21 @@ Namespace Connecting_to_Hierarchical_Data
 				.Position = "Manager"
 			})
 
-			staff_Conflict.Add(New Employee() With {
+			CompanyStaff.Add(New Employee() With {
 				.ID = 14,
 				.ParentID = 4,
 				.Name = "James L. Kelsey",
 				.Department = "Production",
 				.Position = "Manager"
 			})
-			staff_Conflict.Add(New Employee() With {
+			CompanyStaff.Add(New Employee() With {
 				.ID = 15,
 				.ParentID = 4,
 				.Name = "Howard M. Carpenter",
 				.Department = "Production",
 				.Position = "Manager"
 			})
-			staff_Conflict.Add(New Employee() With {
+			CompanyStaff.Add(New Employee() With {
 				.ID = 16,
 				.ParentID = 4,
 				.Name = "Jennifer T. Tapia",
@@ -131,21 +130,21 @@ Namespace Connecting_to_Hierarchical_Data
 				.Position = "Manager"
 			})
 
-			staff_Conflict.Add(New Employee() With {
+			CompanyStaff.Add(New Employee() With {
 				.ID = 17,
 				.ParentID = 5,
 				.Name = "Judith P. Underhill",
 				.Department = "Finance",
 				.Position = "Manager"
 			})
-			staff_Conflict.Add(New Employee() With {
+			CompanyStaff.Add(New Employee() With {
 				.ID = 18,
 				.ParentID = 5,
 				.Name = "Russell E. Belton",
 				.Department = "Finance",
 				.Position = "Manager"
 			})
-			Return staff_Conflict
+			Return CompanyStaff
 		End Function
 	End Module
 End Namespace
